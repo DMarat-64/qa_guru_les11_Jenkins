a7147060a24495d7f0b927e651fc0f1695e39e9e
Marat D <Marat@mail.ru>
2025-05-21 00:40:32 +0400

    added TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,5 +9,6 @@ public class TestBase {
         Configuration.browserSize = "1920*1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
