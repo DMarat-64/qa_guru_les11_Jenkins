793dbeb83a8d0e30cfe56e9e71d6391f6df7638e
Marat D <Marat@mail.ru>
2025-05-21 19:25:06 +0400

    added removeBanner

--- src/test/java/helpers/ActionsHelperAds.java
+++ src/test/java/helpers/ActionsHelperAds.java
@@ -4,8 +4,8 @@ import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class ActionsHelperAds {
     public void removeBanner(){
-        executeJavaScript("$('#fixedban').remove()"); //убираем всплывающие баннеры, чтобы не возникла
-        executeJavaScript("$('footer').remove()");
+        //executeJavaScript("$('#fixedban').remove()"); //убираем всплывающие баннеры, чтобы не возникла
+        //executeJavaScript("$('footer').remove()");
         executeJavaScript("$('[id=\"Ad.Plus-970x250-1\"]').remove()");
         executeJavaScript("$('[id=\"adplus-anchor\"]').remove()");//ошибка element click intercepted
     }
