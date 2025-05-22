27b5d7af4196ea5e7a49c9db446e826bcc82952c
Marat D <Marat@mail.ru>
2025-05-23 00:06:20 +0400

    added negativeLoginTest

---
+++ src/test/java/tests/NegativeLoginTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.CompletionFormsPage;
+import pages.components.ResultTableComponent;
+
+public class NegativeLoginTest extends TestBase{
+    CompletionFormsPage completionForms = new CompletionFormsPage();
+    ResultTableComponent resultTableComponent = new ResultTableComponent();
+
+    @Test
+    @Tag("web")
+    @DisplayName("Неправильное заполнение регистрационной формы")
+    void negativeLoginTest () {
+        completionForms.openPage()
+                .setFirstName("Max")
+                .setLastName("Jons")
+                .setEmail("max@jons.com")
+                .setGender("Male")
+                .setDateOfBirth("14", "August","1980")
+                .setHobbiesWrapper("Sports")
+                .setCurrentAddress("Baker Street 1")
+                .clickSubmit();
+        //Проверки
+        resultTableComponent.notsuccessfullyCompletedCase()
+                .checkResult("Student Name", "Max Jons")
+                .checkResult("Student Email", "max@jons.com")
+                .checkResult("Gender", "Male")
+                .checkResult("Date of Birth", "14 August,1980")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Address", "Baker Street 1");
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -37,9 +39,10 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
-    @AfterEach
 
+    @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
